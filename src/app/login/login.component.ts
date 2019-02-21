ec960058d8f123c13aa61f9c54f2e96c208ac80a
Nestor Flores <nestor.flores@flexdigitalhelath.com>
2019-02-21 18:04:28 -0300

    Added route dynamic method

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute} from '@angular/router';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { ApiService } from './api.service';
 import { routerTransition } from '../router.animations';
