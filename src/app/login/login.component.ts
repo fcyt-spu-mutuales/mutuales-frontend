c361722ce842059a5cc42c19a7276581f0914b49
Nestor Flores <nestor.flores@flexdigitalhelath.com>
2019-02-21 14:09:52 -0300

    Added backend call - must improove

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { ApiService } from './api.service';
 import { routerTransition } from '../router.animations';
+import { LoginRequest } from './models/loginRequest';
+import { LoginResponse } from './models/login.response';
 
 @Component({
-    selector: 'app-login',
-    templateUrl: './login.component.html',
-    styleUrls: ['./login.component.scss'],
-    animations: [routerTransition()]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+  animations: [routerTransition()]
 })
 export class LoginComponent implements OnInit {
-    constructor(
-        private translate: TranslateService,
-        public router: Router
-        ) {
-            this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS']);
-            this.translate.setDefaultLang('en');
-            const browserLang = this.translate.getBrowserLang();
-            this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de|zh-CHS/) ? browserLang : 'en');
-    }
+  request: LoginRequest;
+  constructor(private translate: TranslateService, public router: Router, private apiService: ApiService) {
+    this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de', 'zh-CHS']);
+    this.translate.setDefaultLang('es');
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de|zh-CHS/) ? browserLang : 'en');
+    this.request = new LoginRequest();
+  }
 
-    ngOnInit() {}
+  ngOnInit() {}
 
-    onLoggedin() {
-        localStorage.setItem('isLoggedin', 'true');
+  async onLoggedin() {
+    console.log(this.request);
+    const response: LoginResponse = await this.apiService.login(this.request);
+    if (response.success) {
+      localStorage.setItem('isLoggedin', 'true');
+    } else {
+      console.log('Wrong Credentials!!!');
     }
+  }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -4,12 +4,17 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
     imports: [
         CommonModule,
         TranslateModule,
-        LoginRoutingModule],
+        LoginRoutingModule,
+        FormsModule,
+        HttpClientModule],
     declarations: [LoginComponent]
 })
 export class LoginModule {}
