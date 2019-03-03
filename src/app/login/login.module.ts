02094935b158c921d176701f1e38a73ebe6ec2ba
NestorFlores <n.flores@spds.info>
2019-03-03 19:48:27 -0300

    Added bootstrap alerts on login module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 import { FormsModule } from '@angular/forms';
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
         TranslateModule,
         LoginRoutingModule,
         FormsModule,
+        NgbAlertModule,
         HttpClientModule],
     declarations: [LoginComponent]
 })
