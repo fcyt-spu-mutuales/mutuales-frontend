38ca135a50a830de2c893bf7af892abafe136e70
NestorFlores <n.flores@spds.info>
2019-03-07 11:42:29 -0300

    Added enabled field on search

--- src/app/layout/users/models/user.list.request.ts
+++ src/app/layout/users/models/user.list.request.ts
@@ -2,11 +2,13 @@ export class UserListRequest {
   firstName?: string;
   lastName?: string;
   email?: string;
+  enabled?: boolean;
   limit: number;
   offset: number;
 
   constructor() {
     this.limit = 5;
     this.offset = 0;
+    this.enabled = true;
   }
 }
